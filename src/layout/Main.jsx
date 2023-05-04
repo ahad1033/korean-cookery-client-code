c09fac04e5796d970453acdf4444f9ef32b341dd
ahad1033 <a.ahad2232@gmail.com>
2023-05-04 22:37:54 +0600

    banner section added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../pages/shared/header/Header';
 import Footer from '../pages/shared/footer/Footer';
+import BannerSection from '../pages/bannersection/BannerSection';
 
 const Main = () => {
     return (
         <div>
             <Header />
+            <BannerSection />
             <Outlet />
             <Footer />
         </div>
